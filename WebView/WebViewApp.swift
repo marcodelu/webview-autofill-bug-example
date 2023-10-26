a50335a72b48a3ed56bcf633d234487a0e8c6514
mdelucchi <marco.delucchi@lastminute.com>
2023-10-26 12:21:06 +0200

    Initial Commit

---
+++ WebView/WebViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  WebViewApp.swift
+//  WebView
+//
+//  Created by Marco De Lucchi on 26/10/23.
+//
+
+import SwiftUI
+
+@main
+struct WebViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
