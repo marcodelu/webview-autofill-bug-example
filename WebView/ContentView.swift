92aa17702ce0d6ede9462ca1fad91507df70e19d
mdelucchi <marco.delucchi@lastminute.com>
2023-10-26 17:35:17 +0200

    add WebView component

--- WebView/ContentView.swift
+++ WebView/ContentView.swift
@@ -6,16 +6,48 @@
 //
 
 import SwiftUI
+import WebKit
 
 struct ContentView: View {
+    @State private var cartID: String = "";
+    @State private var showWebCacheCompletedAlert = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            VStack {
+                Text("WebView Autofill Test")
+                    .font(.title)
+
+                Spacer()
+
+                Text("Retrieve the Cart ID from Safari or the lastminute.com app and paste it below to open the Checkout WebView")
+                    .multilineTextAlignment(.center)
+                    .padding(.bottom)
+
+                TextField("Cart ID", text: $cartID)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.bottom)
+
+                NavigationLink(destination: WebView(cartID: cartID)) {
+                    Text("Open")
+                }
+                    .buttonStyle(.borderedProminent)
+
+                Spacer()
+
+                Button("Clean Cache", role: .destructive) {
+                    WKWebsiteDataStore.default().removeData(ofTypes: WKWebsiteDataStore.allWebsiteDataTypes(), modifiedSince: Date(timeIntervalSince1970: 0), completionHandler:{
+                        showWebCacheCompletedAlert = true
+                    })
+                }
+                .alert(isPresented: $showWebCacheCompletedAlert) {
+                    Alert(title: Text("Clean Web Cache"), message: Text("Completed!"))
+                }
+                
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
---
+++ WebView/WebView.swift
@@ -0,0 +1,53 @@
+//
+//  WebViewWrapper.swift
+//  WebView
+//
+//  Created by Marco De Lucchi on 26/10/23.
+//
+
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    private let basePath = "https://secure.lastminute.com/hdp/checkout/carts/";
+    let cartID: String;
+
+    private func buildCheckoutURL() -> URL {
+        return URL(string: basePath + cartID)!
+    }
+
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+
+    func makeUIView(context: Context) -> WKWebView {
+        let webView = WKWebView()
+        webView.navigationDelegate = context.coordinator
+        webView.load(URLRequest(url: buildCheckoutURL()))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        // This space can be left blank
+    }
+
+    class Coordinator: NSObject, WKNavigationDelegate {
+        let parent: WebView
+
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+            print("Webview started loading.")
+        }
+
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            print("Webview finished loading.")
+        }
+
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            print("Webview failed with error: \(error.localizedDescription)")
+        }
+    }
+}
